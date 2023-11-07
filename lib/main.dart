68db46c6509da2368aae6f060c8905cf62f7a98a
RudhyDev <rudhy.pereira@gmail.com>
2023-11-07 15:50:12 -0300

    varaivaies de ambinete para asegurar as chaves

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'package:android_flutter_pushnotification/pages/notification_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:android_flutter_pushnotification/pages/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
-  
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseApi().initNotifications();
-
   runApp(const MyApp());
 }
 
